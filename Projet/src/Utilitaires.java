3700695c1c48f5b1896e1eda650fe8749774594f
Alexandre Hardi <alexandre.hardi@gmail.com>
2015-12-04 14:38:25 +0100

    Initialisation

---
+++ Projet/src/Utilitaires.java
@@ -0,0 +1,14 @@
+
+public class Utilitaires {
+	
+	public static int unEntierAuHasardEntre (int valeurMinimale, int valeurMaximale){
+		double nombreReel;
+		int resultat;
+
+		nombreReel = Math.random();
+		resultat = (int) (nombreReel * (valeurMaximale - valeurMinimale + 1))
+								+ valeurMinimale;
+		return resultat;
+	}
+}
+
