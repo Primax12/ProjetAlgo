39f6774048103caf80d0599e5e568623429abace
diegonuja <diegonuja@gmail.com>
2015-12-08 19:24:38 +0100

    Update TestEquipeGuerrier.java

--- Projet/src/TestEquipeGuerrier.java
+++ Projet/src/TestEquipeGuerrier.java
@@ -3,17 +3,26 @@
  */
 public class TestEquipeGuerrier {
 	public static void main (String[] args){
-		Guerrier g1 = new Guerrier("Bernard", 100, 50);
-		Guerrier g2 = new Guerrier("Aernardo", 100, 50);
+		Guerrier g1 = new Guerrier("Bernard", 100);
+		Guerrier g2 = new Guerrier("Aernardo", 100);
 
-		Guerrier g3 = new Guerrier("Airnardo", 100, 50);
+		Guerrier g3 = new Guerrier("Airnardo", 100);
+		
+		g1.setNbrePV(20);
+		g2.setNbrePV(23);
 		
 		EquipeGuerrier e1 = new EquipeGuerrier("BernTeam");
 		e1.ajouterGuerrier(g1);
 		e1.ajouterGuerrier(g2);
 		e1.ajouterGuerrier(g3);
 		
-		System.out.println(e1);
+		//System.out.println(e1);
+		
+		Guerrier[] liste = e1.tableGuerrierSelonForce(1);
+		
+		for (int i = 0 ; i < liste.length; i++){
+			System.out.println(liste[i]);
+		}
 		
 		//g1.donnerPrivilege(1);
 		//g1.donnerPrivilege(4);
