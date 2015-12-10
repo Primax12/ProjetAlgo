66ba643fd7ab66f54547527776a26887f9702792
Alexandre Hardi <alexandre.hardi@gmail.com>
2015-12-10 16:21:06 +0100

    Test

--- Projet/src/Combat.java
+++ Projet/src/Combat.java
@@ -206,7 +206,7 @@ public class Combat {
 		System.out.println("Equipe "+listeEquipe[tour].getNom()+" Choissisez votre combattant: ");
 		afficherListeGuerrier(listeEquipe[tour].getListeGuerriers());
 		
-		System.out.print("Faites votre choix : ");
+		System.out.print("Faites votre choix : 2");
 		int choix = Utilitaires.choixEntierEntre(0, listeEquipe[tour].getNbreGuerrier()-1, "Ce combattant est mort! ");
 		
 		return listeEquipe[tour].selectionner(choix);
