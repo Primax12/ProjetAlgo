5cff7602be6462dc4b5794447674d207e040c0fa
diegonuja <diegonuja@gmail.com>
2015-12-12 17:19:30 +0100

    Update TestGuerrier.java

--- Projet/src/TestGuerrier.java
+++ Projet/src/TestGuerrier.java
@@ -6,68 +6,34 @@ public class TestGuerrier {
 		
 		System.out.println("===== TEST DE LA CLASSE GUERRIER ==== \n");
 		
-		System.out.println("Creation de 3 combatants et affichage de leurs caracteristiques.\n");
-		Guerrier g1 = new Guerrier("PikaClou", 150);
-		Guerrier g2 = new Guerrier("FrappeFort", 180);
-		Guerrier g3 = new Guerrier("Blorky", 90);
+		// TEST DU CONSTRUCTEUR
+		try{
+			System.out.println("\n=== TEST DU CONSTRUCTEUR ===\n");
+			//Guerrier g1 = new Guerrier(null, 100);
+			//Guerrier g2 = new Guerrier("", 100);
+			//Guerrier g3 = new Guerrier("test", 0);
+			//Guerrier g4 = new Guerrier("test", -10);
+			Guerrier g5	= new Guerrier("test", 100);
+			System.out.println("\n=== Test du constructeur Ok ===\n");
+		}
+		catch(IllegalArgumentException e){
+			System.out.println(e.getMessage() + "\nTest Ko");
+		}
 		
-		System.out.println(g1 + "\n" + g2 + "\n" + g3);
 		
-		System.out.println("\n\nDont de privileges aux guerriers : \n");
-		
-		
-		//test
-		g1.donnerPrivilege(1);
-		g1.donnerPrivilege(5);
-		
-		g2.donnerPrivilege(3) ;
-		g2.donnerPrivilege(4) ;
-		
-		g3.donnerPrivilege(2) ;
-		g3.donnerPrivilege(1) ;
-		
-		System.out.println(g1 + "\n" + g2 + "\n" + g3);
-		
-		System.out.println("\n\nRetrait de privileges aux guerriers : \n");
-		
-		g1.retirePrivilege(3) ;
-		
-		g2.retirePrivilege(4) ;
-		
-		g3.retirePrivilege(1) ;
-
-		System.out.println(g1 + "\n" + g2 + "\n" + g3);
-		
-		System.out.println("\n\nTest des Méthodes sur les privileges.\n");
-		System.out.println("Tes de la methode possede");
-		
-		System.out.println("Le Guerrier 1 possede le privilege 0 : "+g1.possede(0) + " Excepcted : true")  ;
-		System.out.println("Le Guerrier 1 possede le privilege 1 : "+g1.possede(1) + " Excepcted : false") ;
-		
-		System.out.println("\t----------\n");
-		
-		System.out.println("\nTes de la methode possedeTous\n");
-		
-		int[] liste = {3,4} ;
-		System.out.println("Le Guerrier 2 possede les privileges 3 et 4 : "+g2.possedeTous(liste) + " Excepcted : false") ;
-		g2.donnerPrivilege(4);
-		System.out.println("Dont du 3e privilege au Guerrier 2...");
-		System.out.println("Le Guerrier 2 possede les privileges 3 et 4 : "+g2.possedeTous(liste) + " Excepcted : true") ;
-		
-		System.out.println("\n\nTest de la methode donnerForceFrappe\n");
-		
-		System.out.println(g1);
-		System.out.println("\nRetrait de 2 PV à PikaClou\n");
-		g1.setNbrePV((g1.getNbrePV())-149);
-		System.out.println(g1);
-		
-		System.out.println(g3);
-		
-		g3.setNbrePV(1);
-		System.out.println("\nIl ne reste que 1PV à Blorky\n");
-		System.out.println(g3);
-
-		
+		try{
+			System.out.println("\n=== TEST DE SUBIRDEGATS ===\n");
+			Guerrier g1 = new Guerrier("test", 100);
+			//g1.subirDegats(-10);
+			//g1.subirDegats(0);
+			g1.subirDegats(10);
+			System.out.println("\n=== Test de subirDegats Ok ===\n");
+		}
+		catch(IllegalArgumentException e){
+			System.out.println(e.getMessage() + "\nTest Ko");
+		}
 	}
 }
+
+
 
